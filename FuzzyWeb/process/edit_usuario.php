7320ca42cd5a204cf80e56be2c0d7800d8c4adc3
luis <luis@luis-Satellite-C55D-A>
2014-11-15 18:54:35 -0430

    Hola, ahora verifica que haya al menos un administrador

--- FuzzyWeb/process/edit_usuario.php
+++ FuzzyWeb/process/edit_usuario.php
@@ -9,20 +9,31 @@ $p = (string)htmlentities($_GET['p']);
 $db = conectar_db();
 $coleccion = $db->usuario;
 $filtro = array("_id" => new MongoId($user));
+$cursor3 = $coleccion->findOne($filtro);
+$tipoUsu= $cursor3["tipo"];
 
 if ($e == "1")
 {
     if ($p == 1)
+    {
         $usertype = "U_Normal";
+        $cursor2 = $coleccion->find(array("tipo" => "U_Administrador"));
+        $numeroUsuariosAdmin = $cursor2->count();
+    }
     elseif ($p == 2)
+    {
         $usertype = "U_Desarrollador";
+        $cursor2 = $coleccion->find(array("tipo" => "U_Administrador"));
+        $numeroUsuariosAdmin = $cursor2->count();
+    }
     elseif ($p == 3)
          $usertype = "U_Administrador";
-    
-    $newdata = array('$set' => array("tipo" => $usertype));
-    $cursor = $coleccion->update($filtro, 
+    if ($numeroUsuariosAdmin > 1 || !isset($numeroUsuariosAdmin) || $tipoUsu != "U_Administrador"){
+        $newdata = array('$set' => array("tipo" => $usertype));
+        $cursor = $coleccion->update($filtro, 
                                  $newdata);
-    header("Location: ../usuarios.php?sec=0&res=ok");
+        header("Location: ../usuarios.php?sec=0&res=ok");
+    }
 
 } elseif ($e == "2")
 {
